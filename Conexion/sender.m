ce1edd0f18e88538533f5b6e66110c8da5924a3e
ivvasel <ivvasel@teleco.upv.es>
2021-05-19 19:03:30 +0200

    Avances con conexion

--- Conexion/sender.m
+++
@@ -1,8 +0,0 @@
-function [] = sender(tcpipClient,msg)
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
-fopen(tcpipClient);
-fwrite(tcpipClient,msg);
-fclose(tcpipClient);
-end
-
