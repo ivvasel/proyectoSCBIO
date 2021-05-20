2f0c56a4bb9dcb4c1634b264a02de54dc75a688b
AndresFerrando <anferna3@teleco.upv.es>
2021-05-20 20:53:14 +0200

    Mejoras jugabilidad
    Se ha incluido un filtrado de la comunicación entre Unity y MATLAB y se ha añadido un contador que ralentiza la salida de la red neuronal
    
    Co-Authored-By: ivvasel <60435098+ivvasel@users.noreply.github.com>

--- prueba_RT.m
+++ prueba_RT.m
@@ -1,26 +1,28 @@
 function [YPred] = prueba_RT()
-system("Tower/Trbajo(v1).exe &");
-pause(5)
 camera = webcam; 
-preview(camera)
-frames = 200; 
+system("Tower/Trbajo(v1).exe &");
+%preview(camera)
+%frames = 200; 
 load clasificador.mat;
 %Establece la conexion con el servidor Unity
 Cliente=connection;
 soltada = false;
-
+YPred='';
+i=0;
 while(true)
-   
     %Acquire frame for processing
-    img = snapshot(camera);
-    img = imresize(img,[227 227]);
+    i=i+1;
 %     filename = 'video.jpg';
 %     imwrite(img,filename);
-%     ds = imageDatastore(filename);
-    [YPred]=classify(clasificador,img)
-    %Función de controles 
-    soltada=controles_partida(Cliente,YPred,soltada);
-    
+%     ds = imageDatastore(filename);   
+    img = snapshot(camera);
+    img = imresize(img,[227 227]);
+    YPred_anterior=YPred;
+    if(mod(i,2)~=0)
+        [YPred]=classify(clasificador,img)
+        %Función de controles 
+        soltada=controles_partida(Cliente,YPred,soltada,YPred_anterior);
+    end
 end 
 clear camera
 
